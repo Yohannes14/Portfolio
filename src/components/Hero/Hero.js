046e722567c46104facdba74115cc67356e733ba
Yohannes <ydegu@admin.crossroads.co>
2023-02-24 14:10:37 +0300

    projects

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <leftSection>
+      <SectionTitle main center>
+        Hello there <br/>
+        I'm Yohannes Degu
+      </SectionTitle>
+      <SectionText>
+        Frontend Developer |react js/react native/css/tailwindcss styles component php
+
+      </SectionText>
+      <Button onClick ={() => window.location ='https://google.com'}>contact</Button>
+    </leftSection>
+
+  </Section>
 );
 
 export default Hero;
